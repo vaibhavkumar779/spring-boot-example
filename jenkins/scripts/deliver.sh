678e94bbcd6fd42e3f5290da47a71dd655a2d7ef
Vaibhav Kumar <93975853+vaibhavkumar779@users.noreply.github.com>
2022-02-05 18:17:01 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,7 +20,4 @@ set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar ${NAME}-${VERSION}.jar
+
