6d772eaa4a7d60602125dd180f0ff5460f035d6d
Vaibhav Kumar <93975853+vaibhavkumar779@users.noreply.github.com>
2022-02-05 17:43:15 +0530

    Update HelloApplication.java

--- src/main/java/com/hello/hello/HelloApplication.java
+++ src/main/java/com/hello/hello/HelloApplication.java
@@ -1,12 +1,21 @@
 package com.hello.hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Hello world!
+ */
+public class App
+{
 
-@SpringBootApplication
-public class HelloApplication {
+    private final String message = "Hello World!";
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
-	}
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
 }
