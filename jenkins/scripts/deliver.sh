2cb605497a38704e5c7c10deb587b33aa0fc9afc
Vaibhav Kumar <93975853+vaibhavkumar779@users.noreply.github.com>
2022-02-06 10:10:37 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,5 +19,5 @@ echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
-
+echo $VERSION
 
