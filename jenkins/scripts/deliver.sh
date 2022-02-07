bf3f61a82a50c756f07be9ac47ae92168f118da2
Vaibhav Kumar <93975853+vaibhavkumar779@users.noreply.github.com>
2022-02-07 17:06:45 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-
-#java -jar target/${NAME}-${VERSION}.jar
-#mvn fabric8:deploy
+java -Dserver.port=8081 -jar target/${NAME}-${VERSION}.jar
